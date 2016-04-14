8045a26721a038cf11dc9fecae7daa0a0dd7ff27
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-04-14 19:48:21 +0900

    test #2 failed

--- components/Counter.js
+++ components/Counter.js
@@ -2,6 +2,11 @@ import React, { Component } from 'React'
 
 export default class Counter extends Component {
   render() {
-    return <p>Clicked: 0 times</p>
+    return (
+      <p>
+        Clicked: 0 times
+        <button>+</button>
+      </p>
+    )
   }
 }
--- test/components/Counter.spec.js
+++ test/components/Counter.spec.js
@@ -4,12 +4,18 @@ import { shallow } from 'enzyme'
 import Counter from '../../components/Counter'
 
 function setup(value = 0) {
+  const actions = {
+    onIncrement: expect.createSpy()
+  }
+
   const component = shallow(
     <Counter value={value} />
   )
 
   return {
     component: component,
+    actions: actions,
+    buttons: component.find('button'),
     p: component.find('p')
   }
 }
@@ -18,5 +24,11 @@ describe('Counter component', () => {
   it('should display count', () => {
     const { p } = setup()
     expect(p.text()).toMatch(/^Clicked: 0 times/)
+  })
+
+  it('first button should call onIncrement', () => {
+    const { buttons, actions } = setup()
+    buttons.at(0).simulate('click')
+    expect(actions.onIncrement).toHaveBeenCalled()
   })
 })
