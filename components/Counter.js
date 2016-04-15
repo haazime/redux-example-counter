4c01417262581eb1fdeda4c465ba6f01f61c11e9
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-04-15 12:50:09 +0900

    test#5 pass

--- components/Counter.js
+++ components/Counter.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'React'
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.incrementIfOdd = this.incrementIfOdd.bind(this)
+  }
+
+  incrementIfOdd() {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement()
+    }
+  }
+
   render() {
     const { value, onIncrement, onDecrement } = this.props
     return (
@@ -11,7 +22,7 @@ export default class Counter extends Component {
         {' '}
         <button onClick={onDecrement}>-</button>
         {' '}
-        <button>Increment if odd</button>
+        <button onClick={this.incrementIfOdd}>Increment if odd</button>
       </p>
     )
   }
