4bcb6ef4b8e652d5b82bf9f4fcc8b729ea63cc5f
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-04-15 12:26:17 +0900

    test#3 pass

--- components/Counter.js
+++ components/Counter.js
@@ -2,12 +2,14 @@ import React, { Component } from 'React'
 
 export default class Counter extends Component {
   render() {
-    const { value, onIncrement } = this.props
+    const { value, onIncrement, onDecrement } = this.props
     return (
       <p>
         Clicked: 0 times
         {' '}
         <button onClick={onIncrement}>+</button>
+        {' '}
+        <button onClick={onDecrement}>-</button>
       </p>
     )
   }
