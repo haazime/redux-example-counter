429e8930b5669bafa32edf1ea203c689e5c02eee
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-04-15 14:01:54 +0900

    test#7 pass

--- components/Counter.js
+++ components/Counter.js
@@ -4,12 +4,17 @@ export default class Counter extends Component {
   constructor(props) {
     super(props)
     this.incrementIfOdd = this.incrementIfOdd.bind(this)
+    this.incrementAsync = this.incrementAsync.bind(this)
   }
 
   incrementIfOdd() {
     if (this.props.value % 2 !== 0) {
       this.props.onIncrement()
     }
+  }
+
+  incrementAsync() {
+    setTimeout(this.props.onIncrement, 1000)
   }
 
   render() {
@@ -23,6 +28,8 @@ export default class Counter extends Component {
         <button onClick={onDecrement}>-</button>
         {' '}
         <button onClick={this.incrementIfOdd}>Increment if odd</button>
+        {' '}
+        <button onClick={this.incrementAsync}>Increment async</button>
       </p>
     )
   }
